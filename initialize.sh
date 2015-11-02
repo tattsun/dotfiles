ff6b2835cac418a0f417f531351458864ff443e5
tattsun <t.t.mc1192.sf@gmail.com>
2015-11-02 22:37:35 +0900

    test

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-ln -s ~/dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/dotfiles/.emacs.d ~/
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+cp ./git-subcommands/* $(git --exec-path)
